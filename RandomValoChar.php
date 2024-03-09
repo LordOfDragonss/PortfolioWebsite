9c18e65085dfcaf1ce440f10865e4cd67b967b90
MikeTool <miketooltje@gmail.com>
2024-03-09 16:35:57 +0100

    Icons for the generator

--- RandomValoChar.php
+++ RandomValoChar.php
@@ -1,33 +1,35 @@
 <?php
     $name = "";
+    $icon_src = "";
 
 function RandomChar(){
     $jsonData = file_get_contents('json/agents.json');
     $json = json_decode($jsonData,true);
     $NrAgents = count($json['agents']);
     $rand = rand(1,$NrAgents);
-
-
     foreach($json['agents'] as $agent){
         if($rand == $agent['nr']){
-            return $agent['name'];
+            $GLOBALS['name'] = $agent['name'];
+            $GLOBALS['icon_src'] = $agent['icon_src'];
+            return $agent;
         }
     }
 
+
 }
 if(isset($_GET['button'])){
-    $name = RandomChar();
+   RandomChar();
 }
 
 ?>
 
 <!doctype html>
-<html lang="<?php echo $html_lang ?>">
+<html>
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
-    <meta name="author" content="MikeTool" />
+    <meta name="author" content="Mike Tool" />
     <title>Random Character generator</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
@@ -39,12 +41,22 @@ if(isset($_GET['button'])){
     <link href="css/styles.css" rel="stylesheet" />
 </head>
     <body>
-
-<h1 class="text-center"><?php if($name != null) echo $name?></h1>
-
-<form method="get">
-    <button class="btn btn-primary" type="submit" name="button">Generate</button>
-</form>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center"><img class="img-fluid " src="<?php echo $icon_src ?>" alt="..." /><?php if($name != null) echo $name?></h1>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6 text-center">
+                <form method="get">
+                <button class="btn btn-primary" type="submit" name="button">Generate</button>
+                </form>
+            </div>
+            <div class="col-3"></div>
+        </div>
+    </div>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
