de8328b5ff9f676d088ce5ab5d56e6f5e0a6fbf6
Mike Tool <miketooltje@gmail.com>
2025-05-03 15:59:24 +0200

    Fade away start of description

--- gamepages/fade_away.php
+++ gamepages/fade_away.php
@@ -1,8 +1,27 @@
 <?php
 include("gamepage.php");
 ?>
+                <h2>Summary</h2>
                     <p class="text-white-50">
                         Fade away was a solo school project.
+                        The project had as task to design a game from scratch using the subjects we learned troughout the classes.
+
+                        The games main design point was of off the word "Fade" and made into a stealth game prototype.
+                        One of the main design ideas was to make a stealth game with "world breaking" abilities, ablilities that makes it feel like you weren't playing the game as intended.
+                    </p>
+
+
+                <h2>Original concept</h2>
+                    <p class="text-white-50">
+                        The original concept was created by using a random words website, the random words would serve as the "Core mechanic" of the game.
+                        "Fade" instantly stood out to me as a word, with it i saw something like some fading out ghost or something fading away. which I found as something fun to make in something like a stealth game.
+
+                        the first core mechanic diagram looked like this
+
+                        <img src="../assets/gamepages/fadeaway/Fade away main mechanic tree.png"></img>
+
+                        With this concept a saw games like stealth games to be 
+
                     </p>
                 </div>
             </div>
